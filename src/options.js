29ac0f285fc8616cc4118541d3748379145e9948
ehmicky <ehmicky@gmail.com>
2020-04-12 13:16:43 +0200

    Fix undefined options

--- src/options.js
+++ src/options.js
@@ -1,3 +1,4 @@
+import filterObj from 'filter-obj'
 import isPlainObj from 'is-plain-obj'
 import { validate } from 'jest-validate'
 
@@ -6,11 +7,17 @@ export const handleOpts = function (opts, defaultOpts, exampleConfig) {
   validateBasicOpts(opts)
   validate(opts, { exampleConfig })
 
-  return { ...defaultOpts, ...opts }
+  const optsA = filterObj(opts, isDefined)
+  const optsB = { ...defaultOpts, ...optsA }
+  return optsB
 }
 
 export const validateBasicOpts = function (opts) {
   if (!isPlainObj(opts)) {
     throw new TypeError(`Options must be a plain object: ${opts}`)
   }
+}
+
+const isDefined = function (key, value) {
+  return value !== undefined
 }

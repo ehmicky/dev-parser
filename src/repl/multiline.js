6fc97c4e3607415c93c957297862cffa121318a6
ehmicky <ehmicky@gmail.com>
2023-10-22 18:30:13 +0100

    Fix linting

--- src/repl/multiline.js
+++ src/repl/multiline.js
@@ -1,7 +1,7 @@
 // Can do multiline input by ending lines with a backslash
-export const isMultiline = (command) => command[command.length - 1] === '\\'
+export const isMultiline = (command) => command.at(-1) === '\\'
 
 export const handleMultiline = (command) =>
-  command.replace(MULTILINE_REGEXP, '')
+  command.replaceAll(MULTILINE_REGEXP, '')
 
 const MULTILINE_REGEXP = /\\\s*$/gmu
